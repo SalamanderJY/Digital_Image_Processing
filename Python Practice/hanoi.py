0f70b97b348d209c2a586e18aad07b41298b6426
wangjianyuhit@gmail.com <wangjianyuhit@gmail.com>
2017-09-10 20:10:50 +0800

    Hanoi Problem.

---
+++ Python Practice/hanoi.py
@@ -0,0 +1,10 @@
+# -*- coding: UTF-8 -*-
+
+
+def move(n, a, b, c):
+    if n == 1:
+        print(a + '-->' + c)
+        return
+    move(n-1, a, c, b)
+    move(n, a, b, c)
+    move(n-1, b, a, c)
