a05d81faced6b81a0a32c8240b4fa15fed341c9f
wangjianyuhit@gmail.com <wangjianyuhit@gmail.com>
2017-12-09 21:48:21 +0800

    Experiment 3 PSNR and SSIM.

--- DIP/EX3/test.py
+++ DIP/EX3/test.py
@@ -12,23 +12,32 @@ bmp.resolve('head24.bmp')
 result = bmp.bmp2gray()
 cv.imwrite('gray.bmp', result)
 
+for i in range(1, 4):
+    gn = GaussianNoise()
+    gn.addGaussianNoise(result, 0, i * 5)
 
-gn = GaussianNoise()
-gn.addGaussianNoise(result, 0, 15)
+    spn = SaltPepperNoise()
+    spn.addSaltPepperNoise(result, 1 - 0.05 * i)
 
-spn = SaltPepperNoise()
-spn.addSaltPepperNoise(result, 0.95)
+    meanfilter = MeanFilter()
+    meanfilter.filter('GaussianNoise.bmp')
 
-meanfilter = MeanFilter()
-meanfilter.filter('GaussianNoise.bmp')
+    medianfilter = MedianFilter()
+    medianfilter.filter('SaltPepperNoise.bmp')
 
-medianfilter = MedianFilter()
-medianfilter.filter('SaltPepperNoise.bmp')
+    source = cv.imread('gray.bmp', 0)
+    meanfilter_result = cv.imread('MeanFilter.bmp', 0)
+    medianfilter_result = cv.imread('MedianFilter.bmp', 0)
 
+    psnr_mean = ev.psnr(source, meanfilter_result)
+    ssim_mean = ev.ssim(source, meanfilter_result)
+    print('Mean Filter:')
+    print('Gussian Means and Sigma:', 0, i * 5)
+    print('PSNR: %f, SSIM: %f' % (psnr_mean, ssim_mean))
 
-source = cv.imread('gray.bmp', 0)
-meanfilter_result = cv.imread('MeanFilter.bmp', 0)
-psnr = ev.psnr(source, meanfilter_result)
-ssim = ev.ssim(source, meanfilter_result)
-print(psnr)
-print(ssim)
+    psnr_median = ev.psnr(source, medianfilter_result)
+    ssim_median = ev.ssim(source, medianfilter_result)
+    print('Median Filter:')
+    print('SNR:', 1 - 0.05 * i)
+    print('PSNR: %f, SSIM: %f' % (psnr_median, ssim_median))
+
